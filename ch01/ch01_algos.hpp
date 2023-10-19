236d2d920157f16e77751fa5a2b3411c2bee3754
Lucas Peres <lucasperes95@gmail.com>
2023-10-19 16:33:22 -0300

    put const and & in maxMap argument (hpp file)

--- ch01/ch01_algos.hpp
+++ ch01/ch01_algos.hpp
@@ -9,7 +9,7 @@
 int patternCount(const std::string& text, const std::string& pattern);
 std::unordered_set<std::string> frequentWords(const std::string& text, int k);
 std::unordered_map<std::string, int> frequencyTable(const std::string& text, int k);
-int maxMap(std::unordered_map<std::string, int> freqMap);
+int maxMap(const std::unordered_map<std::string, int>& freqMap);
 std::forward_list<std::string> betterFrequentWords(const std::string& text, int k);
 std::string reverseComplement(const std::string& pattern);
 std::forward_list<int> patternMatch(const std::string& pattern, const std::string& genome);
