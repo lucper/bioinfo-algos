5a6f6f5eb90f44d2df3f46844ff3bd9e3b1df6df
Lucas Peres <lucasperes95@gmail.com>
2023-12-03 02:01:04 -0300

    added soln to Path to Genome problem

--- ch03/ch03_algos.cpp
+++ ch03/ch03_algos.cpp
@@ -7,3 +7,11 @@ std::vector<std::string> kmerComposition(const std::string& text, int k) {
         kmers.push_back(text.substr(i, k));
     return kmers;
 }
+
+std::string pathToGenome(const std::vector<std::string>& path) {
+    std::string genome = path[0];
+    int k = path[0].length();
+    for (int i = 1; i < path.size(); ++i)
+        genome += path[i].substr(k - 1, 1);
+    return genome;
+}
--- ch03/ch03_algos.hpp
+++ ch03/ch03_algos.hpp
@@ -2,3 +2,4 @@
 #include <vector>
 
 std::vector<std::string> kmerComposition(const std::string& text, int k);
+std::string pathToGenome(const std::vector<std::string>& kmers);
